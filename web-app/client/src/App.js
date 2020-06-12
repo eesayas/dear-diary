2ada64b25abe9a4b35a512ded5f930eea8861a7e
amreesalmonte <amreesalmonte@gmail.com>
2020-06-12 16:38:28 -0600

    rearranged functions

--- web-app/client/src/App.js
+++ web-app/client/src/App.js
@@ -6,7 +6,7 @@ const webkitSpeechRecognition = window.webkitSpeechRecognition
 const SpeechRecognition = window.SpeechRecognition
 const Speech = SpeechRecognition || webkitSpeechRecognition
 const recognition = new Speech()
-
+/*
 recognition.continous = true
 recognition.interimResults = true
 recognition.lang = 'en-US'
@@ -25,12 +25,38 @@ recognition.addEventListener('result', event => {
 
 recognition.addEventListener('end', recognition.start)
 recognition.start();
-
-function redirect() {
-  history.push('/gallery')
-}
-
+*/
 class App extends Component {
+  state = {
+    transcript: ""
+  }
+
+  componentDidMount() {
+    this.recognizeSpeech();
+  }
+
+  redirect = () => {
+    history.push("/gallery");
+  }
+
+
+  recognizeSpeech = () => {
+    recognition.addEventListener('result', event => {
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join("")
+
+      console.log(transcript);
+      if (transcript.includes("hey")) {
+        this.redirect();
+      }
+    });
+
+    recognition.addEventListener('end', recognition.start)
+    recognition.start();
+  }
+
   render() {
     return (
       <div>
@@ -40,7 +66,7 @@ for handsfree writing.</p>
         <p>*Make sure to enable your microphone</p>
         <p>Get Started</p>
         <p>Say ‘Hey Diary, Get Started’ to well...get started</p>
-        <button type="button" onClick={redirect}>Get Started</button>
+        <button type="button" onClick={this.redirect}>Get Started</button>
       </div >
     );
   }
