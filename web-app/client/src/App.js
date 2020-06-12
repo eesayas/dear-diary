07a800c0f2aab3d481bd65cec6b83bdbf6933f34
amreesalmonte <amreesalmonte@gmail.com>
2020-06-12 14:36:30 -0600

    added routing

--- web-app/client/src/App.js
+++ web-app/client/src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import Main from "./components/Main"
+import Gallery from "./components/Gallery";
+import history from "./history";
+
+const webkitSpeechRecognition = window.webkitSpeechRecognition
+const SpeechRecognition = window.SpeechRecognition
+const Speech = SpeechRecognition || webkitSpeechRecognition
+const recognition = new Speech()
+
+recognition.continous = true
+recognition.interimResults = true
+recognition.lang = 'en-US'
+
+recognition.addEventListener('result', event => {
+  const transcript = Array.from(event.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("")
+
+  console.log(transcript);
+  if (transcript.includes("hey")) {
+    redirect();
+  }
+});
+
+recognition.addEventListener('end', recognition.start)
+recognition.start();
+
+function redirect() {
+  history.push('/gallery')
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Main />
-      </div>
+        <p>Dear Diary</p>
+        <p>Dear Diary is a text-to-speech web application
+for handsfree writing.</p>
+        <p>*Make sure to enable your microphone</p>
+        <p>Get Started</p>
+        <p>Say ‘Hey Diary, Get Started’ to well...get started</p>
+        <button type="button" onClick={redirect}>Get Started</button>
+      </div >
     );
   }
 }
