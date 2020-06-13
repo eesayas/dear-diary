d46969ce6793771204a1b53656413aa9f0742b41
eesayas <briones@ualberta.ca>
2020-06-13 13:51:43 -0600

    login system added

--- web-app/client/src/screens/Login.jsx
+++ web-app/client/src/screens/Login.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
-import "../styling/Login.css"
+import history from "../history";
+import "../styling/Login.css";
+import api from '../api';
 
 class Login extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChangeUsername = async event => {
+        const username = event.target.value;
+        this.setState({username});
+    }
+
+    handleChangePassword = async event => {
+        const password = event.target.value;
+        this.setState({password});
+    }
+
+    handleSubmit = async event => {
+        const { username, password } = this.state;
+        const data = { username, password };
+        await api.loginUser(data).then(res => {
+            if(res.status === 200){
+                history.push('/gallery');
+            } else{
+                alert('There was a problem with login');
+            }
+        });
+    }
+
     render() {
+        const { username, password } = this.state;
         return(
             <div className="login-cont">
                 <div className="header">
@@ -10,11 +44,11 @@ class Login extends Component {
                     <div className="subtitle note">Sorry you need to type this one</div>
                 </div>
                 <form>
-                    <input type="text" name="username" placeholder="USERNAME"></input>
-                    <input type="password" name="password" placeholder="PASSWORD"></input>
+                    <input type="text" name="username" placeholder="USERNAME" value={username} onChange={this.handleChangeUsername}></input>
+                    <input type="password" name="password" placeholder="PASSWORD" value={password} onChange={this.handleChangePassword}></input>
                 </form>
                 <div className="footer">
-                <button className="get-started-btn ui button" type="button">Sign In</button>
+                <button className="get-started-btn ui button" type="button" onClick={this.handleSubmit}>Sign In</button>
                     <p className="note">Don't have an account? <a href="/register">Sign Up</a></p>
                 </div>
             </div>
