60b55d6fbf5544b437819f71d831dfedea13780e
eesayas <briones@ualberta.ca>
2020-06-13 21:41:18 -0600

    added data from server to cards

--- web-app/client/src/screens/Gallery.jsx
+++ web-app/client/src/screens/Gallery.jsx
@@ -3,17 +3,41 @@ import api from '../api';
 import auth from '../auth';
 import '../styling/Gallery.css';
 import history from "../history";
+import moment from 'moment';
 
 class Gallery extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            posts: []
+        }
+    }
+
     componentDidMount = async() => {
         await api.fetchPosts(auth.user).then(data => {
             let posts = data.data.data; //this is all the posts of user
+            this.setState({
+                posts: posts
+            });
         }).catch(err => {
             alert('There has been an error in fetching posts');
         });
     }
 
     render() {
+        const { posts } = this.state;
+        const listItems = posts.reverse().map((item) =>
+            <div className="post-content">
+                <div className="previews-cont">
+                    <div className="preview-title">{ (item.title.length > 30) ? item.title.substring(0, 30) + '...' :  item.title }</div>
+                    <div className="preview-subtitle">{ (item.body.length > 30) ? item.body.substring(0, 30) + '...' :  item.body }</div>
+                </div>
+                <div className="timestamp">
+                    {moment(item.updatedAt).format('MMMM D YYYY') + ' at ' + moment(item.updatedAt).format('h:mma')}
+                </div>
+            </div>
+        );
+        
         return (
             <div className="gallery-cont">
                 <div className="gallery-header">
@@ -23,44 +47,7 @@ class Gallery extends Component {
                     </div>
                 </div>
                 <div className="posts-cont">
-                    <div className="post-content">
-                        <div className="previews-cont">
-                            <div className="preview-title">I don't know what...</div>
-                            <div className="preview-subtitle">Whatever, same old same old...</div>
-                        </div>
-                        <div className="timestamp">
-                            June 11 2020 at 3:45pm
-                        </div>
-                    </div>
-                    <div className="post-content">
-                        <div className="previews-cont">
-                            <div className="preview-title">I don't know what...</div>
-                            <div className="preview-subtitle">Whatever, same old same old...</div>
-                        </div>
-                        <div className="timestamp">
-                            June 11 2020 at 3:45pm
-                        </div>
-                    </div>
-
-                    <div className="post-content">
-                        <div className="previews-cont">
-                            <div className="preview-title">I don't know what...</div>
-                            <div className="preview-subtitle">Whatever, same old same old...</div>
-                        </div>
-                        <div className="timestamp">
-                            June 11 2020 at 3:45pm
-                        </div>
-                    </div>
-
-                    <div className="post-content">
-                        <div className="previews-cont">
-                            <div className="preview-title">I don't know what...</div>
-                            <div className="preview-subtitle">Whatever, same old same old...</div>
-                        </div>
-                        <div className="timestamp">
-                            June 11 2020 at 3:45pm
-                        </div>
-                    </div>
+                    {listItems}
                 </div>
                 <div className="gallery-footer">
                     <button className="ui button get-started-btn" onClick={() => history.push("/compose")}>+ COMPOSE</button>
