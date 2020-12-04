58e128ad3ca9271e51dbd2d467b0c142285d1265
eesayas <briones@ualberta.ca>
2020-12-04 14:43:51 -0700

    added transcript provisions for compose.js

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import UIfx from 'uifx';
+import alertAudio from '../sounds/wake.wav';
 
 const Compose = (props) => {
+    const alert = new UIfx(alertAudio);
+    const wake = [
+      "okay diary", "ok diary", "dear diary", "hey diary", "hello diary", "hi diary", "sup diary"
+    ];
+  
+    const [woke, setWoke] = useState(false);
+    // configure commands
+    const commands = wake.map(w => ({
+      command: w,
+      callback: ({ resetTranscript }) => { alert.play(); setWoke(true); resetTranscript() },
+    }));
+
+    const { transcript, finalTranscript, resetTranscript } = useSpeechRecognition({commands});
     return(
         <div className="h-100 w-100 border fixed-top" hidden={props.hidden}>
-            hello
+            {transcript}
         </div>
     );
 }
